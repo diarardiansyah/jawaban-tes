33e3db4d2f2d40559b26b044ef3c2e0920af72ea
diarardiansyah <ardiansyahdiar@gmail.com>
2021-02-20 21:02:33 +0700

    mengubah data di file 1.php.php

--- 1.Php.php
+++ 1.Php.php
@@ -2,7 +2,7 @@
 
 class Biodata {
 
-	public $name = "Diar";
+	public $name = "Diar Ardiansyah";
 	public $address = "Jl Chentiong 1 RT 04/09 Depok West Java";
 	public $hoobies = ["gaming", "football", "movie", "programming"];
 	public $is_married = FALSE;
