62dad0796f3bc596a3c8d58935e74865c8943c50
diarardiansyah <ardiansyahdiar@gmail.com>
2021-02-20 23:13:12 +0700

    mengubah file 1.php.php

--- 1.Php.php
+++ 1.Php.php
@@ -2,11 +2,12 @@
 
 class Biodata {
 
+
 	public $name = "Diar Ardi";
 	public $address = "Jl Chentiong 1 RT 04/09 Depok West Java";
-	public $hoobies = ["gaming", "football", "movie", "programming"];
+	public $hoobies = ["football", "movie", "programming"];
 	public $is_married = FALSE;
-	public $school = "Pamulang University";
+	public $school = "Universitas Pamulang";
 	public $skills = ["Php", 80, "Html", 85, "Css", 80];
 
 public function __construct($name, $address, $hoobies, $is_married, $school, $skills) {
